7f0b8564734694757503308c76862242bb07bb2c
Olly Chadwick <ollychadwick@gmail.com>
2019-09-25 16:00:14 +0100

    .

--- todos_project/settings.py
+++ todos_project/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=n1nn7zxhcs-l7^@z(=!85jeafjc$d6&*v-$!d+s*q-fk*(y5='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
